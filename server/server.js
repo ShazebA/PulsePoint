6608c344f797b5d2a0e2eb5446010df7267853d6
ShazebA <sashiqu@uwo.ca>
2024-02-16 21:09:00 -0500

    Set up server architecture

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors())
+app.use(express.static(path.join(__dirname, '..', 'client','build')));
+app.use(express.json())
+
+let port = 3002;
+
+
+
+mongoURI = "mongodb+srv://sashiqu:pDQlIbc6YWWXSgnI@deerhacks2024.ldpmy7a.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../olli/build', 'index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log('Express started on port: ', port);
+    });
+    
