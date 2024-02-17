4ac5ca16028a18408d0ba898603e64dcfaf47dc4
Sebastian Vega <sebasvega4604@gmail.com>
2024-02-17 02:37:30 -0500

    auth works, user schema added

---
+++ server/schemas/userSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    healthcardHASH: {
+        type: String,
+        required: false
+    },
+    passwordHASH: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        trim: true
+    },
+    familyClinicID: {
+        type: Number,
+        required: false
+    }
+});
