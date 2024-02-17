a40651981cd932959a85aaead4dcbca473740728
Sebastian Vega <sebasvega4604@gmail.com>
2024-02-17 14:14:24 -0500

    working on auth redirects

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const providerConfig = {
+  domain: "https://dev-8ytwn8nglpbeo6k7.us.auth0.com",
+  clientId: "o0UNRj0w4vAbiQQLqxkiYQkh6vphHKu2",
+  redirectUri: "http://localhost:3000/dashboard"
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      {...providerConfig}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
