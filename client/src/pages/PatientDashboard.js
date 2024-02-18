cdce9f0cdbfd6be796f999b6c0f71ab440a4a61f
Aleksandar Bursac <bursaca@sheridancollege.ca>
2024-02-18 04:41:22 -0500

    more patient dashboard

--- client/src/pages/PatientDashboard.js
+++ client/src/pages/PatientDashboard.js
@@ -12,12 +12,21 @@ const PatientDashboard = ({ user }) => {
         </div> */}
         <div class="date-range-card">
           <h2>Date Range</h2>
-          <h1>This Month</h1>
+          <div class="titles">
+            <svg class="icon" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path d="M1 8.48611H20.25M5.27778 1V3.13889M15.9722 1V3.13889M4.42222 20.25H16.8278C18.0257 20.25 18.6247 20.25 19.0822 20.0169C19.4846 19.8118 19.8118 19.4846 20.0169 19.0822C20.25 18.6247 20.25 18.0257 20.25 16.8278V6.56111C20.25 5.36322 20.25 4.76427 20.0169 4.30674C19.8118 3.90428 19.4846 3.57707 19.0822 3.37202C18.6247 3.13889 18.0257 3.13889 16.8278 3.13889H4.42222C3.22434 3.13889 2.62538 3.13889 2.16785 3.37202C1.76539 3.57707 1.43818 3.90428 1.23313 4.30674C1 4.76427 1 5.36322 1 6.56111V16.8278C1 18.0257 1 18.6247 1.23313 19.0822C1.43818 19.4846 1.76539 19.8118 2.16785 20.0169C2.62538 20.25 3.22433 20.25 4.42222 20.25Z" stroke="#6A6A6A" stroke-width="1.58" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <h1>This Month</h1>
+          </div>
         </div>
 
         <div class="health-info-card">
           <h2>Health Info</h2>
           <h1>Your Health Statistics</h1>
+          <div class="health-info-data">
+            <p>Health Card ID: 7423-713-042-KA</p>
+            <p>Scoliosis: Present</p>
+          </div>
         </div>
 
         <div class="bottom-cards-container">
